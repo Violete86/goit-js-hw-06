f5a992b3f63572d11833d1558c78d2ffb5e48f28
Violete86 <115045476+Violete86@users.noreply.github.com>
2023-05-22 12:41:17 +0200

    change the conditions for function task 06

--- js/task-06.js
+++ js/task-06.js
@@ -3,10 +3,14 @@ const input= document.querySelector('input');
 
 input.addEventListener('blur', (event) => {
   let inputLength = input.value.length;
-   if(inputLength === 6)
+
+   if(inputLength === 6){
+  event.target.classList.remove("invalid");
   event.target.classList.add("valid");
-  else  if(inputLength === 0)
-  event.target.classList.remove('valid','invalid');
+
+} else  if(inputLength === 0)
+  event.target.classList.remove('valid');
+  
   else 
    event.target.classList.add("invalid");
-});
+}); 
